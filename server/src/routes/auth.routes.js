4f38e6b84847ea0aa920962903e51f7159e77ce8
CARLOS MANUEL ESPINOZA MENDOZA <2020371019@gmail.com>
2024-08-10 11:10:04 -0600

    rutas_Eliminadas

--- server/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as authCtrl from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.post('/signin', authCtrl.signIn);
-router.post('/signup', authCtrl.signUp);
-
-export default router;

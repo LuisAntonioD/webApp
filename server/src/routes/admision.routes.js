c4e32a21be9e9e59fe5823c12cb74d3fed8f395f
CARLOS MANUEL ESPINOZA MENDOZA <2020371019@gmail.com>
2024-08-10 11:28:39 -0600

    rutas_aniadidas

---
+++ server/src/routes/admision.routes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+
+const router = Router();
+
+/* 
+  #####                                                                       
+ #     #  ####  #    # ##### #####   ####  #      #      ###### #####   ####  
+ #       #    # ##   #   #   #    # #    # #      #      #      #    # #      
+ #       #    # # #  #   #   #    # #    # #      #      #####  #    #  ####  
+ #       #    # #  # #   #   #####  #    # #      #      #      #####       # 
+ #     # #    # #   ##   #   #   #  #    # #      #      #      #   #  #    # 
+  #####   ####  #    #   #   #    #  ####  ###### ###### ###### #    #  ####  
+                                                                              
+*/
+import * as admisionCtrl from '../controllers/admision.controller.js';
+
+/*
+ #     #                                                                  
+ ##   ## # #####  #####  #      ###### #    #   ##   #####  ######  ####  
+ # # # # # #    # #    # #      #      #    #  #  #  #    # #      #      
+ #  #  # # #    # #    # #      #####  #    # #    # #    # #####   ####  
+ #     # # #    # #    # #      #      # ## # ###### #####  #           # 
+ #     # # #    # #    # #      #      ##  ## #    # #   #  #      #    # 
+ #     # # #####  #####  ###### ###### #    # #    # #    # ######  ####  
+*/
+import { authJwt } from "../middlewares/index.js";
+
+
+//Establecer ruta admision mediante el metodo GET
+router.get('/',admisionCtrl.getAdmisiones);
+router.get('/:admisionId', admisionCtrl.getAdmisionById);
+router.post('/',[authJwt.verifyToken, authJwt.isAdmin], admisionCtrl.createAdmision);
+router.put('/:admisionId', [authJwt.verifyToken, authJwt.isAdmin],admisionCtrl.updateAdmision);
+router.delete('/:admisionId',[authJwt.verifyToken, authJwt.isAdmin], admisionCtrl.deleteAdmision);
+// Nueva ruta para relacionar una oferta educativa con admisiones
+router.get('/:admisionId/ofertas', admisionCtrl.getRelatedOffers);
+router.get('/:admisionId/offers', admisionCtrl.getOffersByAdmisionId)
+
+
+export default router;
